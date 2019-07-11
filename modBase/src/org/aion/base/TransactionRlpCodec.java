664ba981ba66fd98ececcb79388d9ffd0910743e
geoffrey <geoffrey@aion.network>
2019-07-11 16:41:31 -0400

    changed encoding to use same method

--- modBase/src/org/aion/base/TransactionRlpCodec.java
+++ modBase/src/org/aion/base/TransactionRlpCodec.java
@@ -70,23 +70,7 @@ public class TransactionRlpCodec {
 
     /** For signatures you have to keep also RLP of the transaction without any signature data */
     public static byte[] getEncodingNoSignature(AionTransaction tx) {
-        byte[] nonce = RLP.encodeElement(tx.getNonce());
-
-        byte[] to;
-        if (tx.getDestinationAddress() == null) {
-            to = RLP.encodeElement(null);
-        } else {
-            to = RLP.encodeElement(tx.getDestinationAddress().toByteArray());
-        }
-
-        byte[] value = RLP.encodeElement(tx.getValue());
-        byte[] data = RLP.encodeElement(tx.getData());
-        byte[] timeStamp = RLP.encodeElement(null);
-        byte[] nrg = RLP.encodeLong(tx.getEnergyLimit());
-        byte[] nrgPrice = RLP.encodeLong(tx.getEnergyPrice());
-        byte[] type = RLP.encodeByte(tx.getTargetVM());
-
-        return RLP.encodeList(nonce, to, value, data, timeStamp, nrg, nrgPrice, type);
+        return getEncodingPrivate(tx, false);
     }
 
     public static byte[] getEncoding(AionTransaction tx) {
